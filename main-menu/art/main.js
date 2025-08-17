049abd755ba1fb100293f80417aed0e5db3ecb12
Sunny <gnovatosarchive@gmail.com>
2025-08-17 01:21:39 -0400

    Add files via upload

--- main-menu/art/main.js
+++ main-menu/art/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
   if (localStorage.getItem('musicActivated') === 'true') {
-    const music = new Audio('menuSong.mp3');
+    const music = new Audio('https://sunnyglitchue.github.io/soulless-funkmist-archive-web/cdn/predator/Inst%20(4).ogg');
     music.loop = true;
     music.play();
   }
